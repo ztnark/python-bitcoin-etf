999cd4b260e8937be5364641d6cab794568a1fcb
Jose Herrera <jmhm2012@gmail.com>
2017-03-09 17:09:04 -0700

    Add files via upload
    added email functionality

--- etf.py
+++ etf.py
@@ -1,15 +1,52 @@
 import urllib2
 import threading
- 
-def checkETF():
-    threading.Timer(90.0, checkETF).start()
-    target_url = "https://www.sec.gov/rules/sro/batsbzx.htm"
-    data = urllib2.urlopen(target_url).read(20000) #set to only check first 20000 chars
+import os
+import sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
+import smtplib
+
+def main():
+    if len(sys.argv)!=4:
+        print "please include the arguments: fromEmail,fromEmailPW,toEmail."
+        sys.exit()
+
+    url="https://www.sec.gov/rules/sro/batsbzx.htm"
+    fromEmail=sys.argv[1]
+    fromEmailPW=sys.argv[2]
+    toEmail=sys.argv[3]
+    checkETF(url,fromEmail,fromEmailPW,toEmail)
+
+def sendEmail(emailtext,fromEmail,fromEmailPW,toEmail):
+    msg = MIMEMultipart('alternative')
+    msg['To'] = toEmail
+    msg['Subject'] = "Bitcoin etf resuls!"
+    msg['From'] = formataddr((str(Header('Python BTC SEC ETF Bot :)', 'utf-8')), fromEmail))
+    part1 = MIMEText(emailtext, 'plain')
+    msg.attach(part1)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(fromEmail, fromEmailPW)
+    server.sendmail(fromEmail, toEmail, msg.as_string())
+    server.close()
+    print "successfully sent the mail"
+
+def checkETF(url,fromEmail,fromEmailPW,toEmail):
+
+    threading.Timer(15, checkETF,[url,fromEmail,fromEmailPW,toEmail]).start()
+    data = urllib2.urlopen(url).read(20000) #number of chars that should catch the announcement
     eachWord = data.split()
- 
+
     if "Bitcoin" in eachWord:
-        print "Found"
+        text= "Bitcoin has been detected on"+url
+        print text
+        sendEmail(text,fromEmail,fromEmailPW,toEmail)
+        os._exit(0)
     else:
-        print "Nothing"
- 
-checkETF()
+        print "not found yet"
+
+main()
+
--- readme.txt
+++ readme.txt
@@ -1,12 +1,8 @@
 A very simple python script that checks the SEC website for updates on the Bitcoin ETF. 
 
-Tired of pressing "F5" all day waiting for an update from the SEC before March 11th 2017? This simple script will automate that process for you and tell whether or not there has been an update on the Bitcoin ETF. 
+Tired of pressing "F5" all day waiting for an update from the SEC before March 11th 2017? This simple script will automate that process for you and tell whether or not there has been an update on the Bitcoin ETF, plus you'll be emailed.
 
 Step 1:  Downloand and install Python.     
-Step 2:  Open up Python IDLE.    
-Step 3:  Go to File-> New File.     
-Step 4:  Copy & Paste the contents of https://github.com/coinables/python-bitcoin-etf/blob/master/etf.py into the new file you just created.  
-Step 5:  Save
-Step 6:  Go to Run-> Run Module or Press F5 to run
+Step 2:  run as: python etf.py <from email> <from email password> <to email>
 
-
+You will get an error if your email account doen't allow for apps like this to access it, for gmail turn this on (and then off after), at "Less secure apps" section https://www.google.com/settings/security/lesssecureapps
